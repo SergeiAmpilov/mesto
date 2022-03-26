3cf66f7de3fc48c4f10f4f209cfba48c96d691b4
Sergei Ampilov <sergei.ampilov@gmail.com>
2022-03-26 10:07:02 +0300

    feat: класс Section создан

---
+++ scripts/Section.js
@@ -0,0 +1,28 @@
+class Section {
+
+    constructor({ items, renderer }, containerSelector) {
+        this._renderedItems = items;
+        this._container = document.querySelector(containerSelector);
+        this._renderer = renderer;
+    }
+
+    _clear() {
+        this._container.innerHTML = '';
+    }
+
+    renderItems() {
+        this._clear();
+    
+        this._renderedItems.forEach(item => {
+          this._renderer(item);
+        });
+    }
+
+    addItem(element) {
+        this._container.append(element);
+    }
+
+}
+
+
+export { Section };
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import { Card } from './Card.js';
 import { FormValidator } from './FormValidator.js';
+import { Section } from './Section.js';
 
 
 const buttonEditor = document.querySelector('.profile__pen');
